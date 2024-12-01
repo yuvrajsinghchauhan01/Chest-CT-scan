0e0b0f7abaf59070035b112db194f0ac95278723
yuvrajsinghchauhan <ychauhan191@gmail.com>
2024-12-01 13:28:10 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,45 @@
+# template for image classification
+
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s:")
+
+project_name = "chest-ct-scan"
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "dvc.yaml",
+    "app.py",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
+
+for file_path in list_of_files:
+    file_path = Path(file_path)
+    file_dir, file_name = os.path.split(file_path)
+
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+        logging.info(f"Creating directory: {file_dir} for file: {file_name}")
+
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {file_path}")
+
+    else:
+        logging.info(f"{file_name} is already exists")
